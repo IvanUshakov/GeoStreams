55a443c1cfbc051a121cc229f736416417fdfa40
Ivan Ushakov <Ivan.MoLoT@gmail.com>
2012-06-16 16:09:42 +0400

    Initial Commit

---
+++ geosocial/GSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GSAppDelegate.h
+//  geosocial
+//
+//  Created by Иван Ушаков on 16.06.12.
+//  Copyright (c) 2012 ООО Скрипт. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ geosocial/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  geosocial
+//
+//  Created by Иван Ушаков on 16.06.12.
+//  Copyright (c) 2012 ООО Скрипт. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GSAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GSAppDelegate class]));
+    }
+}
